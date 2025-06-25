2cbea5539182603175bdac0f147ee589b0c42470
Lord Liberté <GhostRg110@gmail.com>
2025-06-25 16:07:53 +0200

    Agregar archivos de proyecto.

---
+++ Dia2/Tipos_Datos_IntFloatDouble/Tipos_Datos_IntFloatDouble/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Tipos_Datos_IntFloatDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Numeros enteros -- integer numbers (int)
+            int num1 = 13;
+            int num2 = 7;
+            int suma = num1 + num2;
+            Console.WriteLine("La suma de " + num1 + " más " + num2 + " como números enteros da como resultado " + suma);
+            
+
+            // Numeros de punto flante -- floating point numbers (float)
+            float num3 = 13.5f;
+            float num4 = 7.4f;
+            float sumaFloat = num3 + num4;
+            Console.WriteLine(sumaFloat);
+
+            // Numeros de punto flante de doble precision -- double precision floating point numbers (double)
+            double num5 = 13.544;
+            double num6 = 7.348;
+            double sumaDouble = num5 + num6;
+            Console.WriteLine(sumaDouble);
+        }
+    }
+}
---
+++ Dia2/Tipos_Datos_IntFloatDouble/Tipos_Datos_IntFloatDouble/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Tipos_Datos_IntFloatDouble")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tipos_Datos_IntFloatDouble")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("28d88ba6-0b4f-46e2-b71e-d99401098b0f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
