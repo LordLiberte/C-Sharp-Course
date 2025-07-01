26fe294aca1acf2a8e77ee7a8925a820d7c69584
Lord Liberté <GhostRg110@gmail.com>
2025-07-01 14:51:36 +0200

    Add initial Operadores C# console project
    Created a new C# console application named Operadores targeting .NET Framework 4.7.2. Includes solution and project files, basic Program.cs demonstrating unary negation, and assembly info.

---
+++ Dia3/Operadores/Operadores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            // Operadores Unarios
+            num3 = -num1; // Negación
+            Console.WriteLine($"Negación de {num1}: {num3}");
+
+
+
+        }
+    }
+}
