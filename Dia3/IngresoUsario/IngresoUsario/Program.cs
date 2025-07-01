377a2238a586a8301287efacca39cc445c7a8764
Lord Liberté <GhostRg110@gmail.com>
2025-07-01 14:17:30 +0200

    Ingreso de Usuario
    Se hace un miniprograma para ingrsar el nombre de un usuario

--- Dia3/IngresoUsario/IngresoUsario/Program.cs
+++ Dia3/IngresoUsario/IngresoUsario/Program.cs
@@ -10,6 +10,11 @@ namespace IngresoUsario
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine($"Hola, {nombre}!");
+
         }
     }
 }
