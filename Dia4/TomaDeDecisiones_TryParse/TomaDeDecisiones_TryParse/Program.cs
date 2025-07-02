0c4a294dfc0e7c183a84683dca979f7bb3d359f2
Lord Liberté <GhostRg110@gmail.com>
2025-07-02 17:50:35 +0200

    Add TomaDeDecisiones_TryParse project and update TryParseMethod example
    Added new TomaDeDecisiones_TryParse C# console project demonstrating decision making with int.TryParse. Updated TryParseMethod/Program.cs to include explanatory comments and improved input parsing example.

---
+++ Dia4/TomaDeDecisiones_TryParse/TomaDeDecisiones_TryParse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomaDeDecisiones_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTemperatura;
+            string temperatura = Console.ReadLine();
+            
+            if (int.TryParse(temperatura, out int numero))
+            {
+                numTemperatura = numero;
+
+                if (numTemperatura < 0)
+                {
+                    Console.WriteLine("Hace frío");
+                }
+                else if (numTemperatura >= 0 && numTemperatura <= 20)
+                {
+                    Console.WriteLine("Hace fresco");
+                }
+                else if (numTemperatura > 20 && numTemperatura <= 27)
+                {
+                    Console.WriteLine("Hace calor");
+                }
+                else
+                {
+                    Console.WriteLine("Hace mucho calor");
+                }
+            }
+            else
+            {
+                numTemperatura = 0; // Valor por defecto si la conversión falla
+                Console.WriteLine("El valor ingresado no es válido. Por favor, ingrese un número entero.");
+            }
+        }
+
+    }
+}
--- Dia4/TryParseMethod/TryParseMethod/Program.cs
+++ Dia4/TryParseMethod/TryParseMethod/Program.cs
@@ -10,6 +10,26 @@ namespace TryParseMethod
     {
         static void Main(string[] args)
         {
+
+            /* El método TryParse se usa para determinar si se puede parsear un string al tipo de dato que queremos. En caso afirmativo, la variable
+             booleana se pone en True y se carga el valor parseado en otra variable (por ejemplo integer). En caso de que no se pueda exito es false
+            y no se carga el valor */
+
+            string input = Console.ReadLine();
+            int numberParsed;
+
+            bool isParsed = int.TryParse(input, out numberParsed);
+
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine($"Is Parsed: {isParsed}");
+            if (isParsed)
+            {
+                Console.WriteLine($"Parsed Number: {numberParsed} como {numberParsed.GetType()}");
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse the input.");
+            }
         }
     }
 }
