56cae55d6ee01eefd83232cb0f3ddc8e9c0ef91e
Lord Liberté <GhostRg110@gmail.com>
2025-07-03 16:56:27 +0200

    Update Program.cs

--- Dia4/If_Anidados/If_Anidados/Program.cs
+++ Dia4/If_Anidados/If_Anidados/Program.cs
@@ -10,6 +10,29 @@ namespace If_Anidados
     {
         static void Main(string[] args)
         {
+
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string username = "";
+
+            Console.Write("Por favor, ingrese su nombre de usuario: ");
+            username = Console.ReadLine();
+
+            if (estaRegistrado)
+            {
+                Console.WriteLine("Hola, usuario registrado.");
+                if (username != "")
+                {
+                    Console.WriteLine($"Hola, {username}.");
+                    if (username.Equals("admin"))  // Equals method for string comparison
+                    {
+                        Console.WriteLine("Bienvenido, administrador.");
+                    }
+                }
+            }
+
+            Console.Read();
+
         }
     }
 }
