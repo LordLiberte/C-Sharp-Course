481138d1ff5ae8443455adff913b4f35bf03d430
Lord Liberté <GhostRg110@gmail.com>
2025-07-05 17:20:40 +0200

    Add Declaracion_Switch project with switch statement example
    Initial commit for the Declaracion_Switch C# console application. Includes a sample program demonstrating the use of the switch statement, along with project and configuration files.

---
+++ Dia4/Declaracion_Switch/Declaracion_Switch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declaracion_Switch
+{
+    internal class Program
+    {
+
+        // Switch es una estructura de control que permite evaluar una expresión y ejecutar diferentes bloques de código según el valor de esa expresión.
+        static void Main(string[] args)
+        {
+
+            int autos = 1;
+
+            autos = int.Parse(Console.ReadLine());
+
+
+            // En función de la variable autos, se ejecutará un bloque de código diferente.
+            switch (autos) 
+            {
+                case 1:
+                    Console.WriteLine("Hay un auto");
+                    break;
+                case 2:
+                    Console.WriteLine("Hay dos autos");
+                    break;
+                case 3:
+                    Console.WriteLine("Hay tres autos");
+                    break;
+                default:
+                    Console.WriteLine("No hay autos");
+                    break;
+            }
+        }
+    }
+}
