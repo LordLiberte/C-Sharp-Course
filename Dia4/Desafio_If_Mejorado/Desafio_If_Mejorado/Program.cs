59903d8955432cb2f51695d19435c29f0579f255
Lord Liberté <GhostRg110@gmail.com>
2025-07-05 17:43:33 +0200

    Add temperature evaluation console app
    Initial commit for Desafio_If_Mejorado, a C# console application that prompts the user for a temperature in Celsius, validates the input, and prints a message based on the temperature range. Includes solution, project files, and implementation.

---
+++ Dia4/Desafio_If_Mejorado/Desafio_If_Mejorado/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_If_Mejorado
+{
+
+    /*
+     
+      Si el valor ingresado no es un integer, debe imprimir en la consola "Esa no es una temperatura válida".
+
+      En caso de que el valor ingresado sí sea un integer válido, entonces debe hacer lo siguiente:
+
+      Si el valor de la temperatura ingresada es <=15 debe escribir "Hace mucho frio" en la consola.
+
+      Si el valor de la temperatura ingresada es entre >= 16 y <=28 debe escribir "Hace un clima agradable" en la consola.
+
+      Si el valor de la temperatura ingresada es > 28 debe escribir "Hace mucho calor" en la consola.
+    
+     */
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string temperaturaIngresada;
+            Console.Write("Ingrese una temperatura en grados Celsius:");
+            temperaturaIngresada = Console.ReadLine();
+
+            bool esInteger = int.TryParse(temperaturaIngresada, out int temperatura);
+
+            string mensaje;
+
+            if (esInteger)
+            {
+                mensaje = (temperatura <= 15) ? "Hace mucho frio" :
+                          (temperatura >= 16 && temperatura <= 28) ? "Hace un clima agradable" :
+                          "Hace mucho calor";
+
+                Console.WriteLine(mensaje);
+
+            }
+
+
+        }
+    }
+}
---
+++ Dia4/Desafio_If_Mejorado/Desafio_If_Mejorado/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Desafio_If_Mejorado")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desafio_If_Mejorado")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("bee1aa88-fbbc-4985-89e0-45c2d2946b30")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
