54aaac6f50e2e77d238f57a0d074cbf4d859d391
Lord Liberté <GhostRg110@gmail.com>
2025-07-05 17:38:35 +0200

    Add examples using ternary operator for conditions
    Introduced code demonstrating the use of the ternary operator to determine the state of water based on temperature and to evaluate exam results. Includes commented-out traditional if-else logic for comparison.

--- Dia4/OperadorTernario_IF_mejorada/OperadorTernario_IF_mejorada/Program.cs
+++ Dia4/OperadorTernario_IF_mejorada/OperadorTernario_IF_mejorada/Program.cs
@@ -10,6 +10,46 @@ namespace OperadorTernario_IF_mejorada
     {
         static void Main(string[] args)
         {
+
+            int temperatura = 30;
+            string estadoDelAgua;
+
+
+            /* Lo que se haría
+            if (temperatura <0)
+            {
+                estadoDelAgua = "Sólido";
+            }
+            else if (temperatura >= 0 && temperatura < 100)
+            {
+                estadoDelAgua = "Líquido";
+            }
+            else
+            {
+                estadoDelAgua = "Gaseoso";
+            }
+
+            */
+
+            // If mejorado variable = condición ? valorSiVerdadero : valorSiFalso;
+
+            estadoDelAgua = (temperatura < 0) ? "Sólido" :
+                            (temperatura >= 0 && temperatura < 100) ? "Líquido" :
+                            "Gaseoso";
+
+            Console.WriteLine($"El estado del agua a {temperatura}°C es: {estadoDelAgua}");
+
+            int resultadoExamen = 8;
+
+            string mensaje;
+
+            mensaje = (resultadoExamen < 4) ? "Aplazado" :
+                      (4 < resultadoExamen && resultadoExamen < 7) ? "Aprobado" :
+                      "Promocionado";
+
+            Console.WriteLine(mensaje);
+
+
         }
     }
 }
