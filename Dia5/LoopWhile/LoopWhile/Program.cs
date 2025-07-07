a7380fbee15478116eb2ddd6846c71751504696b
Lord Liberté <GhostRg110@gmail.com>
2025-07-07 19:07:32 +0200

    Sum even numbers using do-while loop
    Refactored the loop to sum even numbers from 0 to 49 and print each number as it is processed. The final sum is displayed at the end. Improved loop logic to avoid infinite loops and clarified output.

--- Dia5/LoopWhile/LoopWhile/Program.cs
+++ Dia5/LoopWhile/LoopWhile/Program.cs
@@ -11,14 +11,32 @@ namespace LoopWhile
         static void Main(string[] args)
         {
 
-            int contador = 15;
+            int suma = 0;
+
+            int contador = 0;
 
             do
             {
-                Console.WriteLine("Contador: " + contador);
-                contador++;
+                if (contador % 2 == 0)
+                {
+                    suma += contador;
+                    contador++;
+                    Console.WriteLine("El número es: " + contador);
+                }
+                else
+                {
 
-            } while (contador <= 10);
+                    // Sin esto se queda en bucle en 1, ya que 1 no cumple la condición del if y no se suma 1 al contador
+                    contador++;
+                    Console.WriteLine("El número es: " + contador);
+              
+                }
+            } while (contador < 50);
+
+
+
+
+            Console.WriteLine(suma);
         }
     }
 }
