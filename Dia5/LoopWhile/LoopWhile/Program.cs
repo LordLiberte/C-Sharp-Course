a878da8879e26e4c0a52102f2696f6edd5fd8b13
Lord Liberté <GhostRg110@gmail.com>
2025-07-07 18:51:21 +0200

    Add LoopWhile project with do-while loop example
    Created a new Visual Studio solution and project demonstrating the use of a do-while loop in C#. Includes basic project files, configuration, and a Program.cs that prints a counter from 0 to 10.

---
+++ Dia5/LoopWhile/LoopWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+
+            do
+            {
+                Console.WriteLine("Contador: " + contador);
+                contador++;
+
+            } while (contador <= 10);
+        }
+    }
+}
