eb5ab454856d11f4dee9aa5366e566e3d303d4f5
Lord Liberté <GhostRg110@gmail.com>
2025-07-07 18:48:15 +0200

    Implement factorial calculation using for loop
    Replaced the previous sum of even numbers logic with a factorial calculation for the number 5. Added explanatory comments to clarify the factorial computation process.

--- Dia5/LoopFor/LoopFor/Program.cs
+++ Dia5/LoopFor/LoopFor/Program.cs
@@ -11,19 +11,28 @@ namespace LoopFor
         static void Main(string[] args)
         {
 
-            int suma = 0;
+            int numero = 5;
+            int factorial = 1;
 
-            for (int contador = 0; contador < 50; contador++)
+            /* 
+             * 
+             * Lo que hace este bucle es determinar el counter como 1, lo compara al numero para hacer el factorial, si es menor o igual
+             * entonces entra al bucle y multiplica el valor factorial actual por el numero de contador (en el primer caso es 1) entonces
+             * 1x1 es 1, incrementa ahora el counter a 2, haciendo que el factorial ahora sea 1x2, entonces factorial vale 2. 
+             * El contador pasa a valer 3, entonces es 2x3 y factorial pasa a valer 6. El contador pasa a 4 y el factorial pasa a 4x6. 
+             * El contador pasa a 5 y entonces pasa a 24x5
+             * 
+             */
+
+            for (int counter = 1; counter <= numero; counter++)
             {
-                if (contador % 2 == 0)
-                {
-                    suma += contador;
-                }
+
+                factorial = (factorial * counter);
             }
 
 
 
-            Console.WriteLine(suma);
+            Console.WriteLine(factorial);
         }
     }
 }
