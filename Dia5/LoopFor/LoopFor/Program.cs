eb531617819cfcbaa213be1a0a2e2ede2b0dddaf
Lord Liberté <GhostRg110@gmail.com>
2025-07-07 18:30:55 +0200

    Loop For Introduction
    Counter program up to 100.

---
+++ Dia5/LoopFor/LoopFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int contador = 0; contador < 100; contador++)
+            {
+                Console.WriteLine("Contador: " + contador);
+                Console.ReadLine();
+            }
+        }
+    }
+}
