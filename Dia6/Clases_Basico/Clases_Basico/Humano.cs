9948f8cad07266f8baef3631792bffba0cc73c26
Lord Liberté <GhostRg110@gmail.com>
2025-07-10 08:51:05 +0200

    Add Humano class with properties and methods
    Introduced the Humano class with member variables for first name, last name, and age, along with methods for presenting, checking age, and validating names. Updated Program.cs to create and use Humano objects, demonstrating the new functionality. Also updated the project file to include Humano.cs.

--- Dia6/Clases_Basico/Clases_Basico/Humano.cs
+++ Dia6/Clases_Basico/Clases_Basico/Humano.cs
@@ -8,6 +8,52 @@ namespace Clases_Basico
 {
     class Humano
     {
+        // Miembro Variable
+        public string primerNombre;
+        public int edad;
+        public string apellido;
+
+
+        // Miembro método
+
+        public void presentarse()
+        {
+
+            comprobarNombre(primerNombre + " " + apellido);
+
+        }
+
+        public void edadHumano()
+        {
+            Console.WriteLine("Tengo " + edad + " años.");
+        }
+
+
+        public void ComprobarEdad()
+        {
+            if (edad < 18)
+            {
+                Console.WriteLine("Eres menor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Eres mayor de edad.");
+            }
+        }
+
+        public void comprobarNombre(string nombre)
+        {
+            if (nombre == null || nombre == "")
+            {
+                Console.WriteLine("No tienes nombre.");
+            }
+            else
+            {
+                Console.WriteLine("Tu nombre es " + nombre);
+            }
+        }
+
+
 
     }
 }
--- Dia6/Clases_Basico/Clases_Basico/Program.cs
+++ Dia6/Clases_Basico/Clases_Basico/Program.cs
@@ -10,6 +10,22 @@ namespace Clases_Basico
     {
         static void Main(string[] args)
         {
+            // Crear un objeto de la clase Humano
+            // clase nombre = new Clase();
+            Humano juan = new Humano();
+
+            // Asignar un valor a la variable miembro
+            juan.primerNombre = Console.ReadLine();
+            juan.edad = 20; // Asignar un valor a la variable miembro edad
+            juan.edadHumano(); // Llamar al método edadHumano
+            juan.ComprobarEdad(); // Llamar al método ComprobarEdad
+            juan.apellido = "Pérez"; // Asignar un valor a la variable miembro apellido
+            juan.presentarse(); // Llamar al método presentarse
+
+            // Crear otro objeto de la clase Humano
+            Humano maria = new Humano();
+            maria.primerNombre = "María";
+            maria.presentarse();
         }
     }
 }
