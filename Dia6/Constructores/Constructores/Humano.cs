b862af6d7e3dae9bffcb46fa15e4217a32621a34
Lord Liberté <GhostRg110@gmail.com>
2025-07-12 09:11:32 +0200

    Add initial Constructores C# project
    Created a new C# console application named 'Constructores' demonstrating the use of constructors. Includes a 'Humano' class with a constructor and a greeting method, and a main program that instantiates and uses this class.

---
+++ Dia6/Constructores/Constructores/Humano.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    internal class Humano
+    {
+        string nombre;
+        string apellido;
+        int edad;
+
+        // Constructor por defecto. Se ejecuta al crear una instancia de la clase Humano.
+        // Con esto se crea un constructor por defecto. A la instancia habrá que asignarle valores en el programa principal.
+        public Humano(string nombre, string apellido)
+        {
+            this.nombre = nombre;  // Con this se hace referencia a los atributos de la clase a la que asignamos valores de los parámetros del constructor.
+            this.apellido = apellido;
+        }
+
+        /*
+        public void PrimerNombre()
+        {
+            nombre = Console.ReadLine();
+        }
+
+        public void PrimerApellido()
+        {
+             apellido = Console.ReadLine();
+        }
+        */
+        public void Saludar()
+        {
+            Console.WriteLine("Hola " + nombre + " " + apellido);
+        }
+    }
+}
---
+++ Dia6/Constructores/Constructores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Humano human = new Humano(nombre: Console.ReadLine(), apellido: Console.ReadLine());
+            human.Saludar();
+        }
+    }
+}
