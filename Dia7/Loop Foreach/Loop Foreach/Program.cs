e16f43779234a133ab9322e0706c17a279a13641
Lord Liberté <GhostRg110@gmail.com>
2025-07-15 15:17:37 +0200

    Add Loop Foreach C# project with foreach examples
    Added a new C# console application demonstrating the use of foreach loops with arrays of BigInteger and strings. Includes project files, configuration, and a sample program that calculates and prints values using foreach.

---
+++ Dia7/Loop Foreach/Loop Foreach/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics; // Ensure you have the System.Numerics assembly referenced for BigInteger
+
+namespace Loop_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger [] nums = new BigInteger[1000];
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i!= 0)
+                {
+                    nums[i] = (nums[i - 1]) * 3;
+                }
+                else
+                {
+                    nums[i] = 0 + 2;
+                }
+               
+            }
+
+            
+            foreach (BigInteger num in nums)
+            {
+                Console.WriteLine(num*100);
+
+            }
+
+
+            string[] amigos = new string[5] { "Juan", "Pedro", "Maria", "Jose", "Ana" };
+
+            foreach (string amigo in amigos)
+            {
+                Console.WriteLine(amigo);
+            }
+        }
+    }
+}
